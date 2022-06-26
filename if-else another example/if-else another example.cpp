38a8dab9fa828d850a3b57206c7bd8b298d34562
nursenaduyku1@gmail.com <nursenaduyku1@gmail.com>
2022-06-26 19:21:58 +0300

    Proje dosyası ekle.

---
+++ if-else another example/if-else another example.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int a;
+	int b;
+	int c;
+	cout << "enter first number" << endl;
+	cin >> a;
+	cout << "enter second number" << endl;
+	cin >> b;
+	cout << "enter third number" << endl;
+	cin >> c;
+	if (a < b && a < c && b < c)
+	{
+		cout << a << "is the smallest number" << endl;
+		cout << c << "is the biggest number" << endl;
+	}
+	else if (a < b && a < c && c < b)
+	{
+		cout << a << "is the smallest number" << endl;
+		cout << b << "is the biggest number" << endl;
+	}
+	else if (b < a && b < c && a < c)
+	{
+		cout << b << "is the smallest number" << endl;
+		cout << c << "is the biggest number" << endl;
+	}
+	else if (b < a && b < c && c < a)
+	{
+		cout << b << "is the smallest number" << endl;
+		cout << a << "is the biggest number" << endl;
+	}
+	
+	else if (c < a && c < b&&a<b)
+	{
+		cout << c << "is the smallest number" << endl;
+		cout << b << "is the biggest number" << endl;
+	}
+	else if (c < a && c < b && b < a)
+	{
+		cout << c << "is the smallest number" << endl;
+		cout << a << "is the biggest number" << endl;
+	}
+	else
+		cout << "any of the three numbers is equal to another number" << endl;
+
+}
